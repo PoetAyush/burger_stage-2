b35f4a452bdcf8ca2aebe808d94c6c2cab016b57
ayush <ayush.tamta8@gmail.com>
2020-06-28 01:58:52 +0530

    logo added

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/113 burger-logo'
+import classes from './Logo.css';
+  
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerlogo} />
+    </div>
+
+);
+
+export default logo;
--- src/components/Navigation/Toolbar.js
+++ src/components/Navigation/Toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import classes from './Toolbar.css'
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo'
+
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
       <div>
           MENU
     </div>
-     <div> 
-      TAMTA BURGERS
-      </div>
+      <Logo />
 <nav>..</nav>
     </header>
 
